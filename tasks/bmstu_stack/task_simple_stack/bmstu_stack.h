17045e2164891818392c3a6b368e69151792908f
angelevdok <evdokimova.student@gmail.com>
2026-01-21 20:28:22 +0000

    stack

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -12,28 +12,87 @@ class stack
    public:
 	stack() : data_(nullptr), size_(0u) {}
 
-	bool empty() const noexcept { return size_ == 100u; }
+	bool empty() const noexcept { return size_ == 0u; }
 
-	size_t size() const noexcept { return 0; }
+	size_t size() const noexcept { return size_; }
 
-	~stack(yyy) {}
+	~stack() { clear(); }
 
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
+		T* new_data = (T*)::operator new(sizeof(T) * (size_ + 1));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (new_data + i) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (new_data + size_) T(std::forward<Args>(args)...);
+		::operator delete(data_);
+		data_ = new_data;
+		++size_;
 	}
 
-	void push(T&& value) {}
+	void push(T&& value)
+	{
+		T* new_data = (T*)::operator new(sizeof(T) * (size_ + 1));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (new_data + i) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (new_data + size_) T(std::move(value));
+		::operator delete(data_);
+		data_ = new_data;
+		++size_;
+	}
 
-	void clear() noexcept {}
+	void clear() noexcept
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
+			data_[i].~T();
+		}
+		::operator delete(data_);
+		data_ = nullptr;
+		size_ = 0;
+	}
 
-	void push(const T& value) {}
+	void push(const T& value)
+	{
+		T* new_data = (T*)::operator new(sizeof(T) * (size_ + 1));
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (new_data + i) T(data_[i]);
+			data_[i].~T();
+		}
+		new (new_data + size_) T(value);
+		::operator delete(data_);
+		data_ = new_data;
+		++size_;
+	}
 
-	void pop() {}
+	void pop()
+	{
+		if (empty())
+			throw std::underflow_error("stek pustoi");
+		--size_;
+		data_[size_].~T();
+	}
 
-	T& top() { return data_[0]; }
+	T& top()
+	{
+		if (empty())
+			throw std::underflow_error("stek pustoi");
+		return data_[size_ - 1];
+	}
 
-	const T& top() const { return data_[0]; }
+	const T& top() const
+	{
+		if (empty())
+			throw std::underflow_error("stek pustoi");
+		return data_[size_ - 1];
+	}
 
    private:
 	T* data_;
